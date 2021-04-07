ec084ff976b6f46fecaf509363940f0767ff5d4d
Marek Zbroch <marek.zbroch@networktocode.com>
2021-04-07 18:17:40 +0200

    OnboardingDevice creation fix ('Fix Issue #93' / ntc-netbox-plugin-onboarding)

---
+++ nautobot_device_onboarding/migrations/0002_create_onboardingdevice.py
@@ -0,0 +1,20 @@
+from django.db import migrations
+
+
+def create_missing_onboardingdevice(apps, schema_editor):
+    Device = apps.get_model("dcim", "Device")
+    OnboardingDevice = apps.get_model("nautobot_device_onboarding", "OnboardingDevice")
+
+    for device in Device.objects.filter(onboardingdevice__isnull=True):
+        OnboardingDevice.objects.create(device=device)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("nautobot_device_onboarding", "0001_initial"),
+    ]
+
+    operations = [
+        migrations.RunPython(create_missing_onboardingdevice),
+    ]
