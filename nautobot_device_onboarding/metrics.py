a2cbdb3a8cf5c8fd599c7bc5b87923b2bb888de4
scetron <stephen.corry@networktocode.com>
2023-03-24 15:46:27 -0400

    add results metrics gauges!

--- nautobot_device_onboarding/metrics.py
+++ nautobot_device_onboarding/metrics.py
@@ -1,6 +1,33 @@
-"""Plugin additions to the Nautobot navigation menu."""
+"""Plugin metrics."""
+
 from prometheus_client import Counter
+
+from prometheus_client.metrics_core import GaugeMetricFamily
+
+from nautobot_device_onboarding.choices import OnboardingStatusChoices, OnboardingFailChoices
+from nautobot_device_onboarding.models import OnboardingTask, OnboardingDevice
+
+METRICS_PREFIX = "deviceonboarding_"
 
 onboardingtask_results_counter = Counter(
     name="onboardingtask_results_total", documentation="Count of results for Onboarding Task", labelnames=("status",)
 )
+
+def current_onboarding_results():
+    results_gauges = GaugeMetricFamily(f"{METRICS_PREFIX}onboardtask_results_count", "Device Onboarding Task Results", labels=["name"])
+    
+    results_gauges.add_metric(labels=["total"],value=OnboardingTask.objects.count())
+    for status_choice in OnboardingStatusChoices.CHOICES:
+        results_gauges.add_metric(labels=[status_choice[0]], value=OnboardingTask.objects.filter(status=status_choice[0]).count())
+
+    yield results_gauges
+
+def current_onboarding_failures():
+    results_gauges = GaugeMetricFamily(f"{METRICS_PREFIX}onboardtask_failure_count", "Device Onboarding Failures", labels=["name"])
+
+    for failure_choice in OnboardingFailChoices.CHOICES:
+        results_gauges.add_metric(labels=[failure_choice[0]], value=OnboardingTask.objects.filter(failed_reason=failure_choice[0]).count())
+        
+    yield results_gauges
+
+metrics = [current_onboarding_results, current_onboarding_failures]
