1b72478eeb3e0ddfabbca437e5f64d336b4a0ac6
scetron <stephen.corry@networktocode.com>
2023-03-27 09:44:06 -0400

    add docstrings

--- nautobot_device_onboarding/metrics.py
+++ nautobot_device_onboarding/metrics.py
@@ -9,12 +9,16 @@ from nautobot_device_onboarding.models import OnboardingTask, OnboardingDevice
 
 METRICS_PREFIX = "deviceonboarding_"
 
+"""Used in the worker to keep track of total onboarding tasks.
+"""
 onboardingtask_results_counter = Counter(
     name="onboardingtask_results_total", documentation="Count of results for Onboarding Task", labelnames=("status",)
 )
 
 def current_onboarding_results():
-    results_gauges = GaugeMetricFamily(f"{METRICS_PREFIX}onboardtask_results_count", "Device Onboarding Task Results", labels=["name"])
+    """Creates a gauge metric for Onboarding Task Results.
+    """
+    results_gauges = GaugeMetricFamily(f"{METRICS_PREFIX}onboardtask_results_count", "Device Onboarding Task Results", labels=["status"])
     
     results_gauges.add_metric(labels=["total"],value=OnboardingTask.objects.count())
     for status_choice in OnboardingStatusChoices.CHOICES:
@@ -23,7 +27,12 @@ def current_onboarding_results():
     yield results_gauges
 
 def current_onboarding_failures():
-    results_gauges = GaugeMetricFamily(f"{METRICS_PREFIX}onboardtask_failure_count", "Device Onboarding Failures", labels=["name"])
+    """Creates a gauge metric for Onboarding Task failures.
+
+    This only shows the failures, so if none have failed or failures have been deleted, this will return 0.
+    Metric labels are the failure reason.
+    """
+    results_gauges = GaugeMetricFamily(f"{METRICS_PREFIX}onboardtask_failure_count", "Device Onboarding Failures", labels=["failure_code"])
 
     for failure_choice in OnboardingFailChoices.CHOICES:
         results_gauges.add_metric(labels=[failure_choice[0]], value=OnboardingTask.objects.filter(failed_reason=failure_choice[0]).count())
