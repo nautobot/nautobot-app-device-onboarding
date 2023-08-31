7ae189bb35dc6940a5766355409dd6b1afd5609c
scetron <stephen.corry@networktocode.com>
2023-08-31 08:28:13 -0400

    part 1 migration

---
+++ nautobot_device_onboarding/migrations/0004_migrate_site_to_location_part_1.py
@@ -0,0 +1,26 @@
+from django.db import migrations, models
+
+import nautobot.dcim.models
+import nautobot.extras.models 
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("nautobot_device_onboarding", "0003_onboardingtask_label"),
+        ("dcim", "0034_migrate_region_and_site_data_to_locations"),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name="onboardingtask",
+            name="location",
+            field=models.ForeignKey(to="dcim.Location", on_delete=models.SET_NULL, blank=True, null=True),
+        ),
+        migrations.AlterField(
+            model_name="onboardingtask",
+            name="role",
+            field=nautobot.extras.models.RoleField(to="extras.Role", on_delete=models.SET_NULL, blank=True, null=True),
+        ),
+    ]
+
