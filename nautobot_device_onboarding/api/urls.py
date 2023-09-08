cb99739f6244737063e11d2e729ba5540fa31dc2
scetron <stephen.corry@networktocode.com>
2023-09-08 14:05:21 -0400

    update urls

--- nautobot_device_onboarding/api/urls.py
+++ nautobot_device_onboarding/api/urls.py
@@ -1,20 +1,11 @@
 """REST API URLs for device onboarding."""
 
-# from rest_framework import routers
-# from nautobot_device_onboarding.api.views import OnboardingTaskView
-
-# router = routers.DefaultRouter()
-
-# router.register(r"onboarding", OnboardingTaskView)
-
-# urlpatterns = router.urls
-
 from nautobot.apps.api import OrderedDefaultRouter
 
 from nautobot_device_onboarding.api import views
 
 router = OrderedDefaultRouter()
-router.register("onboarding", views.OnboardingTaskView)
+router.register("onboarding", views.OnboardingTaskViewSet)
 
 
 app_name = "nautobot_device_onboarding-api"
