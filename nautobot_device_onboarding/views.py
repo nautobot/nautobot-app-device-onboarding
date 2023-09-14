814938211e9fe15db6daeb7eff879844949e7db4
scetron <stephen.corry@networktocode.com>
2023-09-14 16:09:30 -0400

    move from viewset to specific mixins

--- nautobot_device_onboarding/views.py
+++ nautobot_device_onboarding/views.py
@@ -5,17 +5,17 @@ from django.shortcuts import get_object_or_404, render
 from nautobot.core.views import generic
 
 from nautobot_device_onboarding.filters import OnboardingTaskFilterSet
-from nautobot_device_onboarding.forms import OnboardingTaskForm, OnboardingTaskFilterForm
+from nautobot_device_onboarding.forms import OnboardingTaskForm, OnboardingTaskFilterForm, OnboardingTaskBulkEditForm
 from nautobot_device_onboarding.models import OnboardingTask
 from nautobot_device_onboarding.tables import OnboardingTaskTable
 
 from nautobot_device_onboarding.api.serializers import OnboardingTaskSerializer
 
-from nautobot.apps.views import NautobotUIViewSet
+from nautobot.apps.views import ObjectEditViewMixin, ObjectListViewMixin, ObjectDetailViewMixin, ObjectBulkCreateViewMixin, ObjectBulkDestroyViewMixin, ObjectChangeLogViewMixin, ObjectDestroyViewMixin, ObjectNotesViewMixin
 
-class OnboardingTaskUIViewSet(NautobotUIViewSet):
+class OnboardingTaskUIViewSet(ObjectEditViewMixin, ObjectListViewMixin, ObjectDetailViewMixin, ObjectBulkCreateViewMixin, ObjectBulkDestroyViewMixin, ObjectChangeLogViewMixin, ObjectDestroyViewMixin, ObjectNotesViewMixin):
 
-    # bulk_update_form_class = forms.YourAppModelBulkEditForm
+    bulk_update_form_class = OnboardingTaskBulkEditForm
     filterset_class = OnboardingTaskFilterSet
     filterset_form_class = OnboardingTaskFilterForm
     form_class = OnboardingTaskForm
