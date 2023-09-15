5607f8a3c8e79684d1a979e110665e095888dc08
scetron <stephen.corry@networktocode.com>
2023-09-15 18:26:18 -0400

    fix serializer, add validate step

--- nautobot_device_onboarding/api/serializers.py
+++ nautobot_device_onboarding/api/serializers.py
@@ -1,45 +1,81 @@
 """Model serializers for the nautobot_device_onboarding REST API."""
 # pylint: disable=duplicate-code
-
 from rest_framework import serializers
-
-from nautobot.core.api.serializers import ValidatedModelSerializer
-from nautobot.dcim.models import Location, Platform
-from nautobot.extras.models import Role
+from nautobot.core.api.serializers import NotesSerializerMixin, ValidatedModelSerializer
 
 from nautobot_device_onboarding.models import OnboardingTask
-from nautobot_device_onboarding.utils.credentials import Credentials, onboarding_credentials_serializer
+from nautobot_device_onboarding.utils.credentials import Credentials
 from nautobot_device_onboarding.worker import enqueue_onboarding_task
 
 
-
-class OnboardingTaskSerializer(ValidatedModelSerializer):
+class OnboardingTaskSerializer(NotesSerializerMixin, ValidatedModelSerializer):
     # """Serializer for the OnboardingTask model."""
+
+    username = serializers.CharField(
+        required=False,
+        write_only=True,
+        help_text="Device username",
+    )
+
+    password = serializers.CharField(
+        required=False,
+        write_only=True,
+        help_text="Device password",
+    )
+
+    secret = serializers.CharField(
+        required=False,
+        write_only=True,
+        help_text="Device secret password",
+    )    
 
     class Meta:  # noqa: D106 "Missing docstring in public nested class"
         model = OnboardingTask
 
-        fields = "__all__"
+        fields = [
+            "id",
+            "location",
+            "ip_address",
+            "username",
+            "password",
+            "secret",
+            "port",
+            "timeout",
+            "role",
+            "device_type",
+            "platform",
+            "created_device",
+            "status",
+            "failed_reason",
+            "message",
+            "object_type"
+        ]
 
         extra_kwargs = {"location":{"required": True}}
 
-        read_only_fields = ["id", "created_device", "status", "failed_reason", "message"]
+        read_only_fields = ["id", "created_device", "status", "failed_reason", "message", "object_type"]
 
-    def create(self, validated_data):
-        """Create an OnboardingTask and enqueue it for processing."""
-        # Fields are string-type so default to empty (instead of None)
-        username = validated_data.pop("username", "")
-        password = validated_data.pop("password", "")
-        secret = validated_data.pop("secret", "")
+    def validate(self, data):
+        attrs = data.copy()
+        username = attrs.pop("username", "")
+        password = attrs.pop("password", "")
+        secret = attrs.pop("secret", "")
 
-        credentials = Credentials(
+        self.credentials = Credentials(
             username=username,
             password=password,
             secret=secret,
         )
 
+        return super().validate(attrs)
+
+    def create(self, validated_data):
+        """Create an OnboardingTask and enqueue it for processing."""
+        # Fields are string-type so default to empty (instead of None)
+
         onboarding_task = OnboardingTask.objects.create(**validated_data)
 
-        enqueue_onboarding_task(onboarding_task.id, credentials)
+        enqueue_onboarding_task(onboarding_task.id, self.credentials)
 
         return onboarding_task
+
