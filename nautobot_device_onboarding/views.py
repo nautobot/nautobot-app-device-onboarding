a6b6462923d26f38ff78de20a80da5ceba8093a1
scetron <stephen.corry@networktocode.com>
2023-09-16 11:53:32 -0400

    remove bulkeditform

--- nautobot_device_onboarding/views.py
+++ nautobot_device_onboarding/views.py
@@ -11,7 +11,7 @@ from nautobot.apps.views import (
 )
 
 from nautobot_device_onboarding.filters import OnboardingTaskFilterSet
-from nautobot_device_onboarding.forms import OnboardingTaskForm, OnboardingTaskFilterForm, OnboardingTaskBulkEditForm
+from nautobot_device_onboarding.forms import OnboardingTaskForm, OnboardingTaskFilterForm
 from nautobot_device_onboarding.models import OnboardingTask
 from nautobot_device_onboarding.tables import OnboardingTaskTable
 
@@ -29,7 +29,7 @@ class OnboardingTaskUIViewSet(
     ObjectNotesViewMixin,
 ):
     """UI Viewset for Onboarding."""
-    bulk_update_form_class = OnboardingTaskBulkEditForm
+
     filterset_class = OnboardingTaskFilterSet
     filterset_form_class = OnboardingTaskFilterForm
     form_class = OnboardingTaskForm
