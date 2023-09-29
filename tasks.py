ac63ff4c891aeb8be7d63eaeda13d6013ddc5b86
scetron <stephen.corry@networktocode.com>
2023-09-29 13:23:09 -0400

    updates for 2.0

--- tasks.py
+++ tasks.py
@@ -26,7 +26,7 @@ namespace = Collection("nautobot_device_onboarding")
 namespace.configure(
     {
         "nautobot_device_onboarding": {
-            "nautobot_ver": "2.0.0-rc.4",
+            "nautobot_ver": "2.0.0",
             "project_name": "nautobot_device_onboarding",
             "python_ver": "3.10",
             "local": False,
