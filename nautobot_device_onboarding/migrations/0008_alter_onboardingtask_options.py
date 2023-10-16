df76011722ee5fc0468aa27da9453d60003fa707
Gary Snider <75227981+gsnider2195@users.noreply.github.com>
2023-10-16 21:22:26 +0000

    add docstring and comments

--- nautobot_device_onboarding/migrations/0008_alter_onboardingtask_options.py
+++ nautobot_device_onboarding/migrations/0008_alter_onboardingtask_options.py
@@ -4,6 +4,8 @@ from django.db import migrations
 class Migration(migrations.Migration):
     dependencies = [
         ("nautobot_device_onboarding", "0003_onboardingtask_label"),
+        # This migration does not actually depend on the below migration but it is added here to
+        # prevent Django from failing to migrate due to multiple leaf nodes in the migration graph
         ("nautobot_device_onboarding", "0007_alter_onboardingtask_ip_address"),
     ]
 
