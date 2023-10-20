490bfaa69dde9ed703812d7375b24f36692cbfb9
bakebot <info@networktocode.com>
2023-10-20 09:57:30 +0000

    Cookie initialy baked by NetworkToCode Cookie Drift Manager Tool
    Template:
    
    ```
    {
        "template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
        "dir": "nautobot-app",
        "ref": "develop",
        "path": null
    }
    ```
    
    Cookie:
    
    ```
    {
        "remote": "https://github.com/nautobot/nautobot-plugin-device-onboarding.git",
        "path": "/opt/ntc/drift-manager/outputs/nautobot-plugin-device-onboarding",
        "repository_path": "/opt/ntc/drift-manager/outputs/nautobot-plugin-device-onboarding",
        "dir": "",
        "branch_prefix": "drift-manager",
        "context": {
            "codeowner_github_usernames": "@smith-ntc",
            "full_name": "Network to Code, LLC",
            "email": "info@networktocode.com",
            "github_org": "nautobot",
            "plugin_name": "nautobot_device_onboarding",
            "verbose_name": "Device Onboarding",
            "plugin_slug": "nautobot-device-onboarding",
            "project_slug": "nautobot-plugin-device-onboarding",
            "repo_url": "https://github.com/nautobot/nautobot-plugin-device-onboarding",
            "base_url": "nautobot-device-onboarding",
            "min_nautobot_version": "2.0.0",
            "max_nautobot_version": "2.9999",
            "camel_name": "NautobotDeviceOnboarding",
            "project_short_description": "Device Onboarding",
            "model_class_name": "None",
            "open_source_license": "Apache-2.0",
            "docs_base_url": "https://docs.nautobot.com",
            "docs_app_url": "https://docs.nautobot.com/projects/device-onboarding/en/latest",
            "_template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
            "_output_dir": "/opt/ntc/drift-manager/outputs",
            "_repo_dir": "/opt/ntc/drift-manager/outputs/.cookiecutters/cookiecutter-nautobot-app/nautobot-app",
            "_checkout": "develop"
        },
        "base_branch": "develop",
        "remote_name": "origin",
        "pull_request_strategy": "PullRequestStrategy.CREATE",
        "post_actions": [
            "PostAction.BLACK"
        ],
        "baked_commit_ref": "",
        "draft": true
    }
    ```
    
    CLI Arguments:
    
    ```
    {
        "cookie_dir": "",
        "input": false,
        "json_filename": "setup-cookie-device-onboarding.json",
        "output_dir": "./outputs",
        "push": true,
        "template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
        "template_dir": "nautobot-app",
        "template_ref": "develop",
        "pull_request": null,
        "post_action": [
            "black"
        ],
        "disable_post_actions": false,
        "draft": true
    }
    ```

--- docs/dev/extending.md
+++ docs/dev/extending.md
@@ -1,9 +1,6 @@
 # Extending the App
 
+!!! warning "Developer Note - Remove Me!"
+    Information on how to extend the App functionality.
+
 Extending the application is welcome, however it is best to open an issue first, to ensure that a PR would be accepted and makes sense in terms of features and design.
-
-## Customizing Onboarding Behaviour With Extensions
-
-This plugin provides methods to customize onboarding behaviour. By creating onboarding extensions, it is possible to onboard switch stacks, HA pairs and perform other customizations.
-
-Please see the dedicated FAQ for [device onboarding extensions](onboarding_extensions.md).
--- nautobot_device_onboarding/__init__.py
+++ nautobot_device_onboarding/__init__.py
@@ -1,50 +1,26 @@
 """Plugin declaration for nautobot_device_onboarding."""
-
-try:
-    from importlib import metadata
-except ImportError:
-    # Python version < 3.8
-    import importlib_metadata as metadata
+# Metadata is inherited from Nautobot. If not including Nautobot in the environment, this should be added
+from importlib import metadata
 
 __version__ = metadata.version(__name__)
 
-from nautobot.extras.plugins import PluginConfig
+from nautobot.extras.plugins import NautobotAppConfig
 
 
-class OnboardingConfig(PluginConfig):
+class NautobotDeviceOnboardingConfig(NautobotAppConfig):
     """Plugin configuration for the nautobot_device_onboarding plugin."""
 
     name = "nautobot_device_onboarding"
     verbose_name = "Device Onboarding"
     version = __version__
-    min_version = "2.0.0-rc.4"
-    max_version = "2.9"
     author = "Network to Code, LLC"
-    author_email = "opensource@networktocode.com"
-    description = "Nautobot App that simplifies device onboarding (and re-onboarding) by collecting and populating common device 'facts' into Nautobot."
-    base_url = "device-onboarding"
+    description = "Device Onboarding."
+    base_url = "nautobot-device-onboarding"
     required_settings = []
-    default_settings = {
-        "create_platform_if_missing": True,
-        "create_manufacturer_if_missing": True,
-        "create_device_type_if_missing": True,
-        "create_device_role_if_missing": True,
-        "default_device_role": "network",
-        "default_device_role_color": "ff0000",
-        "default_management_interface": "PLACEHOLDER",
-        "default_management_prefix_length": 0,
-        "default_device_status": "Active",
-        "default_ip_status": "Active",
-        "create_management_interface_if_missing": True,
-        "skip_device_type_on_update": False,
-        "skip_manufacturer_on_update": False,
-        "platform_map": {},
-        "onboarding_extensions_map": {
-            "ios": "nautobot_device_onboarding.onboarding_extensions.ios",
-        },
-        "object_match_strategy": "loose",
-    }
+    min_version = "2.0.0"
+    max_version = "2.9999"
+    default_settings = {}
     caching_config = {}
 
 
-config = OnboardingConfig  # pylint:disable=invalid-name
+config = NautobotDeviceOnboardingConfig  # pylint:disable=invalid-name
