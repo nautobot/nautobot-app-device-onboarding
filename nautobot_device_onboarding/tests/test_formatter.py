e975dc86eee1bad52179d5a40a34ac539381a501
Jeff Kala <jeff.l.kala@gmail.com>
2024-10-03 21:41:30 -0500

    add raw parser and add new sync devices platform hp_comware

--- nautobot_device_onboarding/tests/test_formatter.py
+++ nautobot_device_onboarding/tests/test_formatter.py
@@ -404,7 +404,6 @@ class TestFormatterSyncDevices(unittest.TestCase):
     @patch("nautobot_device_onboarding.nornir_plays.transform.GitRepository")
     def setUp(self, mock_repo):
         # Load the application command_mapper files
-        self.maxDiff = None
         mock_repo.return_value = 0
         self.platform_parsing_info = add_platform_parsing_info()
         self.host = Host(
@@ -428,7 +427,15 @@ class TestFormatterSyncDevices(unittest.TestCase):
 
     def test_add_platform_parsing_info_sane_defaults(self):
         # Note: This is also officially tested in test_transform, but secondary check here as well.
-        default_mappers = ["cisco_ios", "arista_eos", "cisco_wlc", "cisco_xe", "juniper_junos", "cisco_nxos"]
+        default_mappers = [
+            "cisco_ios",
+            "arista_eos",
+            "cisco_wlc",
+            "cisco_xe",
+            "juniper_junos",
+            "cisco_nxos",
+            "hp_comware",
+        ]
         self.assertEqual(sorted(default_mappers), list(sorted(self.platform_parsing_info.keys())))
 
     def test_create_inventory_host_per_platform(self):
@@ -511,6 +518,7 @@ class TestFormatterSyncNetworkDataNoOptions(unittest.TestCase):
     def test_perform_data_extraction_sync_network_data_no_options(self):
         supported_platforms = list(self.platform_parsing_info.keys())
         supported_platforms.remove("cisco_wlc")
+        supported_platforms.remove("hp_comware")
         for platform in supported_platforms:
             self.host.platform = platform
             current_test_dir = f"{MOCK_DIR}/{platform}/"
@@ -578,6 +586,7 @@ class TestFormatterSyncNetworkDataWithVrfs(unittest.TestCase):
     def test_perform_data_extraction_sync_network_data_with_vrfs(self):
         supported_platforms = list(self.platform_parsing_info.keys())
         supported_platforms.remove("cisco_wlc")
+        supported_platforms.remove("hp_comware")
         for platform in supported_platforms:
             self.host.platform = platform
             current_test_dir = f"{MOCK_DIR}/{platform}/"
@@ -645,6 +654,7 @@ class TestFormatterSyncNetworkDataWithVlans(unittest.TestCase):
     def test_perform_data_extraction_sync_network_data_with_vlans(self):
         supported_platforms = list(self.platform_parsing_info.keys())
         supported_platforms.remove("cisco_wlc")
+        supported_platforms.remove("hp_comware")
         for platform in supported_platforms:
             self.host.platform = platform
             current_test_dir = f"{MOCK_DIR}/{platform}/"
@@ -713,6 +723,7 @@ class TestFormatterSyncNetworkDataAll(unittest.TestCase):
     def test_perform_data_extraction_sync_network_data_all(self):
         supported_platforms = list(self.platform_parsing_info.keys())
         supported_platforms.remove("cisco_wlc")
+        supported_platforms.remove("hp_comware")
         for platform in supported_platforms:
             self.host.platform = platform
             current_test_dir = f"{MOCK_DIR}/{platform}/"
