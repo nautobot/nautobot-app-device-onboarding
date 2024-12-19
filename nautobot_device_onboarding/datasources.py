9926194cbb92e4d34dc44a60c12bc4074f0c884a
Leo Kirchner <leo.kirchner@networktocode.com>
2024-12-19 14:34:49 +0100

    improves error handling for git repositories providing command mappers

--- nautobot_device_onboarding/datasources.py
+++ nautobot_device_onboarding/datasources.py
@@ -1,15 +1,44 @@
 """Datasources to override command_mapper yaml files."""
 
+from pathlib import Path
+
 from nautobot.apps.datasources import DatasourceContent
 from nautobot.extras.choices import LogLevelChoices
+
+from nautobot_device_onboarding.constants import (
+    ONBOARDING_COMMAND_MAPPERS_CONTENT_IDENTIFIER,
+    ONBOARDING_COMMAND_MAPPERS_REPOSITORY_FOLDER,
+)
 
 
 def refresh_git_command_mappers(repository_record, job_result, delete=False):  # pylint: disable=unused-argument
     """Callback for gitrepository updates on Command Mapper Repo."""
+    # Since we don't create any DB records we can just ignore deletions.
+    if delete:
+        return
+    if ONBOARDING_COMMAND_MAPPERS_CONTENT_IDENTIFIER not in repository_record.provided_contents:
+        return
     job_result.log(
-        "Successfully Pulled Command Mapper Repo",
-        level_choice=LogLevelChoices.LOG_DEBUG,
+        "Refreshing network sync job command mappers...",
+        level_choice=LogLevelChoices.LOG_INFO,
     )
+    repo_data_dir = Path(repository_record.filesystem_path) / ONBOARDING_COMMAND_MAPPERS_REPOSITORY_FOLDER
+    if not repo_data_dir.exists():
+        # Shouldn't use an f string here as it is a log message.
+        job_result.log(
+            "Command mapper repo folder does not exist. Create a sub folder in the repository at '%'".format(  # pylint: disable=consider-using-fstring
+            ),
+            level_choice=LogLevelChoices.LOG_WARNING,
+        )
+        return
+    try:
+        next(repo_data_dir.glob("*.yml"))
+    except StopIteration:
+        job_result.log(
+            "Command mapper repo folder found, but it doesn't contain any command mapper files. "
+            "They need to have the '.yml' extension.",
+            level_choice=LogLevelChoices.LOG_WARNING,
+        )
 
 
 datasource_contents = [
@@ -17,7 +46,7 @@ datasource_contents = [
         "extras.gitrepository",
         DatasourceContent(
             name="Network Sync Job Command Mappers",
-            content_identifier="nautobot_device_onboarding.onboarding_command_mappers",
+            content_identifier=ONBOARDING_COMMAND_MAPPERS_CONTENT_IDENTIFIER,
             icon="mdi-paw",
             callback=refresh_git_command_mappers,
         ),
