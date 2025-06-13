a09654940e35b161f9f8244613b28b2c77b74a46
Stephen Doiron <53412380+sdoiron0330@users.noreply.github.com>
2025-06-13 14:28:55 -0500

    update forms

--- nautobot_device_onboarding/forms.py
+++ nautobot_device_onboarding/forms.py
@@ -7,15 +7,34 @@ from django import forms
 
 class DiscoveredDeviceFilterForm(NautobotFilterForm):
     model = DiscoveredDevice
+    
     class Meta:
         fields = [
-            "id",
+            "ip_address",
+            "tcp_response",
+            "last_successful_tcp_response",
+            "ssh_response",
+            "last_successful_ssh_response",
+            "ssh_port",
+            "ssh_credentials",
+            "discovered_platform",
+            "location",
+            "namespace",
+            "device_role",
+            "device_status",
+            "interface_status",
+            "ip_address_status"
         ]
 
 class DiscoveredDeviceForm(NautobotModelForm):
 
-    last_successful_tcp_response = forms.DateTimeField(widget=DateTimePicker())
-    last_successful_ssh_response = forms.DateTimeField(widget=DateTimePicker())
+    ssh_credentials = DynamicModelChoiceField(queryset=SecretsGroup.objects.all(), to_field_name="name", required=False, label="SSH Credentials")
+    location = DynamicModelChoiceField(queryset=Location.objects.all(), to_field_name="name", required=False, label="Location")
+    namespace = DynamicModelChoiceField(queryset=Namespace.objects.all(), to_field_name="name", required=False, label="Namespace for IP Address")
+    device_role = DynamicModelChoiceField(queryset=Role.objects.all(), to_field_name="name", required=False, label="Device Role")
+    device_status = DynamicModelChoiceField(queryset=Status.objects.all(), to_field_name="name", required=False, label="Device Status")
+    interface_status = DynamicModelChoiceField(queryset=Status.objects.all(), to_field_name="name", required=False, label="Interface Status") # TODO: filter this properly
+    ip_address_status = DynamicModelChoiceField(queryset=Status.objects.all(), to_field_name="name", required=False, label="IP Address Status") 
 
     class Meta:
         model = DiscoveredDevice
@@ -29,9 +48,11 @@ class DiscoveredDeviceForm(NautobotModelForm):
             "ssh_credentials",
             "discovered_platform",
             "location",
+            "namespace",
             "device_role",
             "device_status",
-            "interface_status"
+            "interface_status",
+            "ip_address_status"
         ]
 
 class DiscoveredDeviceBulkEditForm(NautobotBulkEditForm):
