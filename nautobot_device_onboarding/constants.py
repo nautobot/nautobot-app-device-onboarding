70c301bd70c4fcca30892f8e6ad4a329cecc9b88
Anthony <housepbass@gmail.com>
2025-09-26 15:34:47 -0400

    add all other intf mappings for N9K-C93360YC-FX2

--- nautobot_device_onboarding/constants.py
+++ nautobot_device_onboarding/constants.py
@@ -45,6 +45,7 @@ INTERFACE_TYPE_MAP_STATIC = {
     "FastEthernet": "100base-fx",
     "ethernet": "1000base-t",
     "100/1000/10000/25000 Ethernet": "25gbase-x-sfp28",
+    "1000/10000/25000 Ethernet": "25gbase-x-sfp28",
     "GigabitEthernet": "1000base-t",
 }
 
