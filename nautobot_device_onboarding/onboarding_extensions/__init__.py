519cd9ab075d814c8f4b8af0fc224c2d559540a0
Ludovic BOUÉ <lboue@users.noreply.github.com>
2026-09-24 11:49:07 +0200

    Update __init__.py

--- nautobot_device_onboarding/onboarding_extensions/__init__.py
+++ nautobot_device_onboarding/onboarding_extensions/__init__.py
@@ -1,6 +1,1 @@
 """Onboarding Extensions."""
-
-"onboarding_extensions_map": {
-    "ios": "nautobot_device_onboarding.onboarding_extensions.ios",
-    "aruba_aoscx": "nautobot_device_onboarding.onboarding_extensions.aoscx",
-}
